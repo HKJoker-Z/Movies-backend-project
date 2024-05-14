28764b070fe76ffde81c9cf0286e6e94a12a2838
HKJoker <a.qiqi2019@gmail.com>
2024-05-14 16:32:18 +0800

    updated field to match the frontend

--- demo/src/main/java/com/z2/movies/demo/model/Movie.java
+++ demo/src/main/java/com/z2/movies/demo/model/Movie.java
@@ -1,9 +1,6 @@
 package com.z2.movies.demo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Movie {
@@ -11,18 +8,39 @@ public class Movie {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String title;
-    private String genre;
+    private String type;
+
+    //let description column be longtext type
+    @Column(columnDefinition="TEXT")
+    private String description;
+    private String duration;    //it's salary prev in front
+    private String companyName; //it's "Julian Zhao" by default prev company.name in front
+
+    @Column(columnDefinition="TEXT")
+    private String companyDescription;
+    private String companyEmail;
+    private String companyPhone;
+
     private int year;
     private String language;
     private String country;
     private String director;
     private String localPath;
 
-    public String getLocalPath() {
-        return localPath;
-    }
-
-    public void setLocalPath(String localPath) {
+    public Movie(int id, String title, String type, String description, String duration, String companyName, String companyDescription, String companyEmail, String companyPhone, int year, String language, String country, String director, String localPath) {
+        this.id = id;
+        this.title = title;
+        this.type = type;
+        this.description = description;
+        this.duration = duration;
+        this.companyName = companyName;
+        this.companyDescription = companyDescription;
+        this.companyEmail = companyEmail;
+        this.companyPhone = companyPhone;
+        this.year = year;
+        this.language = language;
+        this.country = country;
+        this.director = director;
         this.localPath = localPath;
     }
 
@@ -45,12 +63,60 @@ public class Movie {
         this.title = title;
     }
 
-    public String getGenre() {
-        return genre;
+    public String getType() {
+        return type;
     }
 
-    public void setGenre(String genre) {
-        this.genre = genre;
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getCompanyDescription() {
+        return companyDescription;
+    }
+
+    public void setCompanyDescription(String companyDescription) {
+        this.companyDescription = companyDescription;
+    }
+
+    public String getCompanyEmail() {
+        return companyEmail;
+    }
+
+    public void setCompanyEmail(String companyEmail) {
+        this.companyEmail = companyEmail;
+    }
+
+    public String getCompanyPhone() {
+        return companyPhone;
+    }
+
+    public void setCompanyPhone(String companyPhone) {
+        this.companyPhone = companyPhone;
     }
 
     public int getYear() {
@@ -85,5 +151,11 @@ public class Movie {
         this.director = director;
     }
 
+    public String getLocalPath() {
+        return localPath;
+    }
 
+    public void setLocalPath(String localPath) {
+        this.localPath = localPath;
+    }
 }
